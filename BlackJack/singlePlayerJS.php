b9a8b8f1ef4ba8b66a255de65e02a6684a579d51
AlbertoST876 <albertost876.ast@gmail.com>
2023-03-10 09:37:15 +0100

    Cambiado nombre de carpeta icons a icon

--- BlackJack/singlePlayerJS.php
+++ BlackJack/singlePlayerJS.php
@@ -17,7 +17,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../assets/css/style.css">
-        <link rel="icon" href="../assets/icons/icon.png">
+        <link rel="icon" href="../assets/icon/icon.png">
         <title>AlbertoST Informática - Casino - BlackJack</title>
 
         <script type="module">
--- play.php
+++ play.php
@@ -22,14 +22,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./assets/css/style.css">
-        <link rel="icon" href="./assets/icons/icon.png">
+        <link rel="icon" href="./assets/icon/icon.png">
         <title>AlbertoST Informática - Casino - Play</title>
     </head>
 
     <body>
         <header>
             <nav>
-                <a class="img" href="./index.php"><img src="./assets/icons/icon.png"></a>
+                <a class="img" href="./index.php"><img src="./assets/icon/icon.png"></a>
                 
                 <ul>
                     <li><a href="./index.php">Inicio</a></li>
